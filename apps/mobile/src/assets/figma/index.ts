37e0409d0cf4e2bc3f91648b738949a5cbc2fde2
pablonification <19623037@std.stei.itb.ac.id>
2026-05-05 13:53:50 +0700

    Fix login brand and Google button

--- apps/mobile/src/assets/figma/index.ts
+++ apps/mobile/src/assets/figma/index.ts
@@ -4,7 +4,6 @@ import educationThumb1 from "./education-thumb-1.png";
 import educationThumb2 from "./education-thumb-2.png";
 import educationThumb3 from "./education-thumb-3.png";
 import educationThumb4 from "./education-thumb-4.png";
-import googleIcon from "./google-icon.png";
 import loginBrand from "./login-brand.svg";
 import profilePhoto from "./profile-photo.png";
 import rewardForestBg from "./reward-forest-bg.png";
@@ -14,7 +13,6 @@ export const figmaAssets = {
   aquaBottle,
   ecodropLogo,
   educationThumbs: [educationThumb1, educationThumb2, educationThumb3, educationThumb4],
-  googleIcon,
   loginBrand,
   profilePhoto,
   rewardForestBg,
